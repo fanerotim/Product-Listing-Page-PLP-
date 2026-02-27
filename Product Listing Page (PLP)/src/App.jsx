4a5b3e664ce4b211dcec81c586df099d1c40ae86
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-27 23:38:06 +0200

    add footer and an empty product page, wrap them in a container and make sure footer sticks to the bottom of the page
    using grid layout for app layout

--- Product Listing Page (PLP)/src/App.jsx
+++ Product Listing Page (PLP)/src/App.jsx
@@ -1,13 +1,17 @@
 import styles from './App.module.scss';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
+import { ProductPage } from './components/ProductPage/ProductPage';
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Footer />
+      <div className={styles.app__container}>
+        <Header />
+        <ProductPage />
+        <Footer />
+      </div>
     </>
   )
 }
---
+++ Product Listing Page (PLP)/src/components/ProductPage/ProductPage.jsx
@@ -0,0 +1,8 @@
+export const ProductPage = () => {
+    return (
+        <>
+            <h1>This is my product page</h1>
+            <h2>Why do I not see this text?</h2>
+        </>
+    )
+}
