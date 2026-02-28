54004d60ab69855d02ca514214545b589601e68c
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-28 10:13:26 +0200

    update Navigation to render categories dynamically
    imports database and renders the categories

--- Product Listing Page (PLP)/src/components/Header/Navigation/Navigation.jsx
+++ Product Listing Page (PLP)/src/components/Header/Navigation/Navigation.jsx
@@ -1,15 +1,16 @@
 import styles from './Navigation.module.scss';
+import { database } from '../../../../database/database';
 
-export const Navigation = () => {
+export const Navigation = ({ handleChangeCategory }) => {
+
     return (
         <nav className={styles.nav}>
             <ul>
-                <li>
-                    Category 1
-                </li>
-                <li>
-                    Category 2
-                </li>
+                {database.map((d => {
+                    return (
+                        <li key={d.id} onClick={() => handleChangeCategory(d.category)}>{d.category}</li>
+                    )
+                }))}
             </ul>
         </nav>
     )
