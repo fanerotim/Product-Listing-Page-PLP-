33fe0cc35c7960a814079892f09895248570fb09
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-02-28 10:12:41 +0200

    add database structure
    no items yet, just structure of the db

---
+++ Product Listing Page (PLP)/database/database.js
@@ -0,0 +1,29 @@
+// product / item structure 
+// {
+//     image: '',
+//     name: '',
+//     description: '',
+//     price: '',
+//     rating: ''
+// }
+
+export const database = [
+    {
+        id: 1,
+        category: 'books',
+        description: '',
+        items: []
+    },
+    {
+        id: 2,
+        category: 'magazines',
+        description: '',
+        items: []
+    },
+    {   
+        id: 3,
+        category: 'dictionaries',
+        description: '',
+        items: []
+    }
+]
