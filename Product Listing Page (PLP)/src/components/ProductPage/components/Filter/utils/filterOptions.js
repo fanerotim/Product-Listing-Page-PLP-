2ec0d81549c6825cebfefa1ec769dc744114cda0
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 22:21:01 +0200

    update filter options. rating was inconsistent and cause render issues

--- Product Listing Page (PLP)/src/components/ProductPage/components/Filter/utils/filterOptions.js
+++ Product Listing Page (PLP)/src/components/ProductPage/components/Filter/utils/filterOptions.js
@@ -3,7 +3,26 @@ export const filterOptions = [
         id: 1,
         value: 'rating',
         label: 'Rating',
-        options: [1, 2, 3, 4, 5]
+        options: [{
+            value: 1,
+            label: 'One'
+        },
+        {
+            value: 2,
+            label: 'Two'
+        },
+        {
+            value: 3,
+            label: 'Three'
+        },
+        {
+            value: 4,
+            label: 'Four'
+        },
+        {
+            value: 5,
+            label: 'Five'
+        }]
     },
     {
         id: 2,
@@ -24,7 +43,7 @@ export const filterOptions = [
         label: 'Language',
         options: [{
             value: 'BG',
-            labbel: 'Bulgarian'
+            label: 'Bulgarian'
         },
         {
             value: 'FR',
