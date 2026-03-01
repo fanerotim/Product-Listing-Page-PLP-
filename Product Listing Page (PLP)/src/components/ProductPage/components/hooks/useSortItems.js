bcfc3624e77de97c67fefdfe0801e230a134921d
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 21:15:34 +0200

    leave a comment to remind myself of enhancement that should be done on the sorting mechanism. also fix an issue whereby I was mutating the original data

--- Product Listing Page (PLP)/src/components/ProductPage/components/hooks/useSortItems.js
+++ Product Listing Page (PLP)/src/components/ProductPage/components/hooks/useSortItems.js
@@ -1,25 +1,25 @@
 export const useSortItems = (sortValue, items) => {
 
-    const itemsToSorted = items.slice(0);
+    const itemsToSort = items.slice(0);
     let sortedItems = [];
 
     // TODO: current there is no logic to sort discounted items properly and also cyrillic items are not sorted / add logic to handle that
 
     switch(sortValue) {
         case 'price-ascending':
-            sortedItems = items.sort((a, b) => a.price - b.price);
+            sortedItems = itemsToSort.sort((a, b) => a.price - b.price);
             break;
         case 'price-descending':
-            sortedItems = items.sort((a, b) => b.price - a.price)
+            sortedItems = itemsToSort.sort((a, b) => b.price - a.price)
             break;
         case 'alphabetical-ascending':
-            sortedItems = items.sort((a, b) => a.name.localeCompare(b.name))
+            sortedItems = itemsToSort.sort((a, b) => a.name.localeCompare(b.name))
             break;
         case 'alphabetical-descending':
-            sortedItems = items.sort((a, b) => b.name.localeCompare(a.name))
+            sortedItems = itemsToSort.sort((a, b) => b.name.localeCompare(a.name))
             break;
         default:
-            return items;
+            return sortedItems;
     }
 
     return sortedItems;
