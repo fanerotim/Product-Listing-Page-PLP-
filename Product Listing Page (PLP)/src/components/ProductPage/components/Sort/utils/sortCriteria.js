1cb4d2e3502109e910e372a34789fc5b8591b978
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 18:18:29 +0200

    update criteria field to label to make it more semantically correct

--- Product Listing Page (PLP)/src/components/ProductPage/components/Sort/utils/sortCriteria.js
+++ Product Listing Page (PLP)/src/components/ProductPage/components/Sort/utils/sortCriteria.js
@@ -1,6 +1,6 @@
 export const sortCriteria = [
-    {id: 1, criteria: 'Price (Low to High)', value: 'price-ascending'},
-    {id: 2, criteria: 'Price (High to Low)', value: 'price-descending'},
-    {id: 3, criteria: 'Alphabetical (A-Z)', value: 'alphabetical-ascending'},
-    {id: 4, criteria: 'Alphabetical (Z-A)', value: 'alphabetical-descending'},
+    { id: 1, label: 'Price (Low to High)', value: 'price-ascending' },
+    { id: 2, label: 'Price (High to Low)', value: 'price-descending' },
+    { id: 3, label: 'Alphabetical (A-Z)', value: 'alphabetical-ascending' },
+    { id: 4, label: 'Alphabetical (Z-A)', value: 'alphabetical-descending' },
 ]
