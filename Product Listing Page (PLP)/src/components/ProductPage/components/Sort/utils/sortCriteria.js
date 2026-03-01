b17a31503403af0d82cc0934bf8e940e7b2c3f12
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 15:34:46 +0200

    add a util that stored sort criteria

---
+++ Product Listing Page (PLP)/src/components/ProductPage/components/Sort/utils/sortCriteria.js
@@ -0,0 +1,6 @@
+export const sortCriteria = [
+    {id: 1, criteria: 'Price (Low to High)'},
+    {id: 2, criteria: 'Price (High to Low)'},
+    {id: 3, criteria: 'Alphabetical (A-Z)'},
+    {id: 4, criteria: 'Alphabetical (Z-A)'},
+]
