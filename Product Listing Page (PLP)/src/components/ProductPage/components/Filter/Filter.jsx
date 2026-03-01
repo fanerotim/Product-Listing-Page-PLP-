291fad251a6f2553566b19bf7e7e8d061c17b59c
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 23:02:37 +0200

    first iteration of filter component / feature for desktop

--- Product Listing Page (PLP)/src/components/ProductPage/components/Filter/Filter.jsx
+++ Product Listing Page (PLP)/src/components/ProductPage/components/Filter/Filter.jsx
@@ -1,7 +1,44 @@
 import styles from './Filter.module.scss';
+import { filterOptions } from './utils/filterOptions';
 
 export const Filter = () => {
+
+
+    // TODO: consider adding aria-pressed attribute
     return (
-        <h1>Filter</h1>
+        <>
+            <h3>Filter</h3>
+            <div
+                className={styles.filter__container}
+            >
+                {filterOptions.map((filter) => (
+                    <article
+                        className={styles.filter__group__container}
+                        key={filter.id}
+                    >
+                        <h4
+                            className={styles.filter__group__container__label}
+                        >
+                            {filter.label}
+                        </h4>
+                        
+                        <div
+                            className={styles.filter__group__options__container}
+                        >
+                            {filter.options.map((option, i) => (
+                                <button
+                                    key={option.value}
+                                    className={styles.filter__group__options__container__option}
+                                >
+                                    {option.label}
+                                </button>
+                            ))}
+                        </div>
+
+                    </article>
+                ))}
+
+            </div>
+        </>
     )
 }
