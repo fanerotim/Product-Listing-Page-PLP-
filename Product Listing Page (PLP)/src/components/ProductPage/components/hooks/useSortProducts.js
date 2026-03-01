9154da4e0ffb2f3834d7ab077728d3ed8cfe23da
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 19:24:00 +0200

    add hook that sorts items with basic algorithm; filtering is not yet ready. for now sorts without filters

---
+++ Product Listing Page (PLP)/src/components/ProductPage/components/hooks/useSortProducts.js
@@ -0,0 +1,24 @@
+export const useSortProducts = (sortValue, items) => {
+
+    const itemsToSorted = items.slice(0);
+    let sortedItems = [];
+
+    switch(sortValue) {
+        case 'price-ascending':
+            sortedItems = items.sort((a, b) => a.price - b.price);
+            break;
+        case 'price-descending':
+            sortedItems = items.sort((a, b) => b.price - a.price)
+            break;
+        case 'alphabetical-ascending':
+            sortedItems = items.sort((a, b) => a.name.localeCompare(b.name))
+            break;
+        case 'alphabetical-descending':
+            sortedItems = items.sort((a, b) => b.name.localeCompare(a.name))
+            break;
+        default:
+            return items;
+    }
+
+    return sortedProducts;
+}
