f6f9920f3bf872ff1825c4f0628da82ccd92e66a
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 21:04:09 +0200

    add a TODO comment to improve the sorting logic, so that discounted items and cyrillic items are sorted properly

--- Product Listing Page (PLP)/src/components/ProductPage/components/hooks/useSortItems.js
+++ Product Listing Page (PLP)/src/components/ProductPage/components/hooks/useSortItems.js
@@ -3,6 +3,8 @@ export const useSortItems = (sortValue, items) => {
     const itemsToSorted = items.slice(0);
     let sortedItems = [];
 
+    // TODO: current there is no logic to sort discounted items properly and also cyrillic items are not sorted / add logic to handle that
+
     switch(sortValue) {
         case 'price-ascending':
             sortedItems = items.sort((a, b) => a.price - b.price);
