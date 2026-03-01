412bb1fad5cfc86966e2615a521a8bb7e930fe5a
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-01 17:31:06 +0200

    add values to sortCriteria options

--- Product Listing Page (PLP)/src/components/ProductPage/components/Sort/utils/sortCriteria.js
+++ Product Listing Page (PLP)/src/components/ProductPage/components/Sort/utils/sortCriteria.js
@@ -1,6 +1,6 @@
 export const sortCriteria = [
-    {id: 1, criteria: 'Price (Low to High)'},
-    {id: 2, criteria: 'Price (High to Low)'},
-    {id: 3, criteria: 'Alphabetical (A-Z)'},
-    {id: 4, criteria: 'Alphabetical (Z-A)'},
+    {id: 1, criteria: 'Price (Low to High)', value: 'price-ascending'},
+    {id: 2, criteria: 'Price (High to Low)', value: 'price-descending'},
+    {id: 3, criteria: 'Alphabetical (A-Z)', value: 'alphabetical-ascending'},
+    {id: 4, criteria: 'Alphabetical (Z-A)', value: 'alphabetical-descending'},
 ]
