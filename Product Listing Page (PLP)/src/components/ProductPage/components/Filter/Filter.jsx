6f5815a20bae182ace2201c181604366079af9e9
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-02 10:45:00 +0200

    consume filter state and filter handler inside filter component, assign handler to each filter option button, add class to selected filter options dynamically

--- Product Listing Page (PLP)/src/components/ProductPage/components/Filter/Filter.jsx
+++ Product Listing Page (PLP)/src/components/ProductPage/components/Filter/Filter.jsx
@@ -1,8 +1,7 @@
 import styles from './Filter.module.scss';
 import { filterOptions } from './utils/filterOptions';
 
-export const Filter = () => {
-
+export const Filter = ({ filterState, filterStateHandler }) => {
 
     // TODO: consider adding aria-pressed attribute
     return (
@@ -21,14 +20,18 @@ export const Filter = () => {
                         >
                             {filter.label}
                         </h4>
-                        
+
                         <div
                             className={styles.filter__group__options__container}
                         >
                             {filter.options.map((option, i) => (
                                 <button
                                     key={option.value}
-                                    className={styles.filter__group__options__container__option}
+                                    onClick={() => filterStateHandler(filter.value, option.value)}
+                                    className={`
+                                        ${styles.filter__group__options__container__option}
+                                        ${filterState[filter.value] === option.value ? styles.filter__group__options__container__option__selected : ''}
+                                        `}
                                 >
                                     {option.label}
                                 </button>
