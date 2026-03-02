5fcee1aa1990b728a9d7ce84651a8c22a3240667
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-02 17:06:03 +0200

    add a conditional check that handles 'rating' value comparison

--- Product Listing Page (PLP)/src/components/ProductPage/components/hooks/useFilterItems.js
+++ Product Listing Page (PLP)/src/components/ProductPage/components/hooks/useFilterItems.js
@@ -2,15 +2,21 @@ export const useFilterItems = (filterState, items) => {
     let filteredItems = [];
 
     const checkIfMatches = (item, key, value) => {
-        // TODO: implement logic for rating as it's array in item and number as a value   
-        return item[key] === value;
+        
+        let itemValue = item[key];
+        
+        if (key === 'rating') {
+            itemValue = item[key].length;
+        } 
+        
+        return itemValue === value;
     }
 
     filteredItems = items.filter((item) => {
 
         for (let key in filterState) {
             const value = filterState[key];
-
+ 
             if (value !== null) {
                 const isMatching = checkIfMatches(item, key, value);
 
@@ -23,5 +29,4 @@ export const useFilterItems = (filterState, items) => {
         }
         return true;
     })
-    console.log(filteredItems);
 }
