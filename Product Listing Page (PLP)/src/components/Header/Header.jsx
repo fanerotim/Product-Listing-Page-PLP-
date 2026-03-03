e45a36ca4b505319aa8c19f6e620edab566204d4
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-03 21:39:03 +0200

    allow header to take up 100vw, add wrapper to restrict logo nad nav items from taking up more than 1200px width

--- Product Listing Page (PLP)/src/components/Header/Header.jsx
+++ Product Listing Page (PLP)/src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import styles from './Header.module.scss';
 import { Navigation } from "./Navigation/Navigation"
 
-export const Header = ({handleChangeCategory, activeCategory}) => {
+export const Header = ({ handleChangeCategory, activeCategory }) => {
     return (
-        <header className={styles.header}>
-            <h1>Logo</h1>
-            <Navigation 
-                handleChangeCategory={handleChangeCategory}
-                activeCategory={activeCategory}
-            />
+        <header className={styles.header__element__wrapper}>
+            <div
+                className={styles.header__element__wrapper__nav__and__logo__container}
+            >
+                <h1>Logo</h1>
+                <Navigation
+                    handleChangeCategory={handleChangeCategory}
+                    activeCategory={activeCategory}
+                />
+            </div>
         </header>
     )
 }
