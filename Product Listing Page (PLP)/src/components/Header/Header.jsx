b7229671716675d65ec2d5304e5f637355593c13
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-03 23:06:23 +0200

    render the logo component

--- Product Listing Page (PLP)/src/components/Header/Header.jsx
+++ Product Listing Page (PLP)/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import styles from './Header.module.scss';
-import { Navigation } from "./Navigation/Navigation"
+import { Navigation } from "./Navigation/Navigation";
+import { Logo } from './Logo/Logo';
 
 export const Header = ({ handleChangeCategory, activeCategory }) => {
     return (
@@ -7,7 +8,7 @@ export const Header = ({ handleChangeCategory, activeCategory }) => {
             <div
                 className={styles.header__element__wrapper__nav__and__logo__container}
             >
-                <h1>Logo</h1>
+                <Logo/>
                 <Navigation
                     handleChangeCategory={handleChangeCategory}
                     activeCategory={activeCategory}
