7aed5713c0af13d0016ebe58f8c345a63d480e87
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-04 09:45:31 +0200

    style item added to card modal

--- Product Listing Page (PLP)/src/components/ProductPage/components/ProductItem/components/ItemAddedModal.jsx
+++ Product Listing Page (PLP)/src/components/ProductPage/components/ProductItem/components/ItemAddedModal.jsx
@@ -1,11 +1,19 @@
 import styles from './ItemAddedModal.module.scss';
+import { FaCheck } from "react-icons/fa";
 
 export const ItemAddedModal = () => {
     return (
         <article
             className={styles.modal__container}
-        >  
-            <p>Item sucessfully added to cart</p>
-        </article>   
+        >
+            <FaCheck className={styles.modal__container__success__icon} />
+            <p
+                className={styles.modal__container__text}
+            >
+                Product added to cart
+            </p>
+
+
+        </article>
     )
 }
