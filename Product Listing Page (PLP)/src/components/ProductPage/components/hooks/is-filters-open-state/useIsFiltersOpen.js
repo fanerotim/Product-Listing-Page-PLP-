efc21ba84b4b7a0c7ba1b1792c6f7455b25a9fde
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-04 21:35:00 +0200

    fix content shift when body scroll is hidden / disabled

--- Product Listing Page (PLP)/src/components/ProductPage/components/hooks/is-filters-open-state/useIsFiltersOpen.js
+++ Product Listing Page (PLP)/src/components/ProductPage/components/hooks/is-filters-open-state/useIsFiltersOpen.js
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useIsFiltersOpen = () => {
     const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+
+    useEffect(() => {
+
+        if (isFiltersOpen) {
+            // make sure content does not shift right when we disable scroll
+            const scrollbarWidth = window.innerWidth - document.body.clientWidth;
+            document.body.style.paddingRight = scrollbarWidth + 'px';
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.paddingRight = '0px';
+            document.body.style.overflow = 'auto';
+        }
+
+    }, [isFiltersOpen])
 
     const isFiltersOpenHandler = () => {
         setIsFiltersOpen((prev) => !prev);
