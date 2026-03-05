8ae894fd1dd7510f5cf925061337a26880897abf
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-03-05 11:16:41 +0200

    style footer

--- Product Listing Page (PLP)/src/components/Footer/Footer.jsx
+++ Product Listing Page (PLP)/src/components/Footer/Footer.jsx
@@ -3,10 +3,27 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <ul>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Terms And Conditions</a></li>
-                <li><a href="#">Privacy Policy</a></li>
+            <ul
+                className={styles.footer__list}
+            >
+                <li className={styles.footer__list__item}>
+                    <a
+                        className={styles.footer__list__item__link}
+                        href="javascript:void(0)">About us
+                    </a>
+                </li>
+                <li className={styles.footer__list__item}>
+                    <a
+                        className={styles.footer__list__item__link}
+                        href="javascript:void(0)">Terms And Conditions
+                    </a>
+                </li>
+                <li className={styles.footer__list__item}>
+                    <a
+                        className={styles.footer__list__item__link}
+                        href="javascript:void(0)">Privacy Policy
+                    </a>
+                </li>
             </ul>
         </footer>
     )
